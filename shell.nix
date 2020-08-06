e47391e7d03d89c6b125f6d5630b5d3c33164a11
ajs124 <git@ajs124.de>
2020-08-06 03:38:51 +0200

    add more dependencies etc

--- shell.nix
+++ shell.nix
@@ -1,12 +1,35 @@
-with import <nixpkgs> { };
+with import <nixpkgs> { }; # TODO: pin nixpkgs to some revision/release
 
 stdenv.mkDerivation {
   name = "go";
   buildInputs = [
-    delve
+    # for out code
     go
+    # for the implementations
+    ## node-wot
+    nodejs
+    nodePackages.npm
+    ## wot-py
+    (python3.withPackages (p: with p; [
+      pip
+      setuptools
+      netifaces
+      numpy
+      sphinx
+      sphinx_rtd_theme
+      tornado
+      virtualenvwrapper
+    ]))
+    ## sane-city
+    maven
+    jdk12
   ];
   shellHook = ''
     export GOPATH=$PWD/gopath
+
+    export SOURCE_DATE_EPOCH=315532800
+    alias pip="PIP_PREFIX='$PWD/.pip_packages' \pip"
+    export PYTHONPATH="$PWD/.pip_packages/lib/${pkgs.python3.libPrefix}/site-packages:$PYTHONPATH"
+    cd src
   '';
 }
