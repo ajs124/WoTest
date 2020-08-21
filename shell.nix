af23bfffad3b07e30d53b7a40ed81c8f4e8db359
ajs124 <git@ajs124.de>
2020-08-21 13:57:12 +0200

    more dependencies

--- shell.nix
+++ shell.nix
@@ -3,8 +3,36 @@ with import <nixpkgs> { };
 stdenv.mkDerivation {
   name = "go";
   buildInputs = [
-    delve
+    # delve
+    # our code
     go
+    # node-wot
+    nodejs
+    nodePackages.npm
+    # sane-city
+    jdk12_headless
+    maven
+    # wot-py
+    (python3.withPackages (p: with p; [
+      setuptools
+      tornado
+      jsonschema
+      six
+      rx
+      python-slugify
+      hbmqtt
+      zeroconf
+
+      # for tests
+      pytest
+      faker
+      aiozeroconf
+      mock
+      pyopenssl
+    ]))
+    # unstable.jetbrains.goland
+    mosquitto
+    curl
   ];
   shellHook = ''
     export GOPATH=$PWD/gopath
