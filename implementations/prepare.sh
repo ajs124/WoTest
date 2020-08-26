9d50113735e10f5ad7620e04c7d1ff079fcdbe34
ajs124 <git@ajs124.de>
2020-08-26 14:53:25 +0200

    implementations/prepare.sh: disable python tests

--- implementations/prepare.sh
+++ implementations/prepare.sh
@@ -20,7 +20,7 @@ mvn install -DskipTests  # FIXME
 
 cd ../wot-py
 python3 setup.py build
-WOTPY_TESTS_MQTT_BROKER_URL=mqtt://127.0.0.1 python3 setup.py test
+#WOTPY_TESTS_MQTT_BROKER_URL=mqtt://127.0.0.1 python3 setup.py test
 rm -rf install
 mkdir install
 pip install -t install .
