15e2b9992b88c1cca2067529ffbcf4f532b0aea2
ajs124 <git@ajs124.de>
2020-09-01 18:07:35 +0200

    implementations/node-wot: replace node-wot-install/package.json with bash
    because npm fails in docker, for some reason

--- implementations/prepare_node-wot.sh
+++ implementations/prepare_node-wot.sh
@@ -4,5 +4,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"/node-wot
 npm install
 npm run bootstrap
 npm run build
-cd ../node-wot-install
-npm install
+cd ..
+rm -rf node-wot-install
+mkdir -p node-wot-install/node_modules/@node-wot
+cd $_
+find ../../../node-wot/packages -maxdepth 1 -mindepth 1 -type d -print0 | \
+    while read -r -d "" package; do ln -s "$package" .; done
