c280f3467502a0a55f11c15d952caa51ebe1bd32
ajs124 <git@ajs124.de>
2020-09-22 03:42:33 +0200

    config: split out into separate file

---
+++ src/config.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/philandstuff/dhall-golang/v5"
+	"github.com/rs/zerolog/log"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	TestTypeProtocol = iota
+	TestTypeContents
+	TestTypeMeasure
+)
+
+type ProtocolTestProperties struct {
+	Mode               uint               `json:"mode"`
+	Protocol           uint               `json:"protocol"`
+	RequestUrl         string             `json:"requestUrl"`
+	AuthenticationData AuthenticationData `json:"authentication"`
+}
+
+type ContentTestProperties struct{}
+type MeasureTestProperties struct{}
+
+type Test struct {
+	Timeout                uint                   `json:"timeoutSec"`
+	Path                   string                 `json:"path"`
+	Type                   uint                   `json:"type"`
+	ProtocolTestProperties ProtocolTestProperties `json:"protocolTestProperties"`
+	ContentTestProperties  ContentTestProperties  `json:"contentTestProperties"`
+	MeasureTestProperties  MeasureTestProperties  `json:"measureTestProperties"`
+}
+
+type WoTImplementation struct {
+	Name    string  `dhall:"name"`
+	Path    string  `dhall:"path"` // this is the "install" or "library" path, not the path of the tests
+	Runtime Runtime `dhall:"runtime"`
+}
+
+type Config struct {
+	TestsDir           string              `dhall:"testsDir"`
+	ImplementationsDir string              `dhall:"implementationsDir"`
+	LogLevel           int8                `dhall:"logLevel"`
+	Implementations    []WoTImplementation `dhall:"implementations"`
+}
+
+func loadConfig(configPath string) (Config, error) {
+	var config Config
+	bytes, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return config, err
+	}
+	err = dhall.Unmarshal(bytes, &config)
+	if err == nil {
+		log.Debug().Msgf("Loaded Config: %+v", config)
+	}
+	return config, err
+}
+
+func loadTestIndex(path string) ([]Test, error) {
+	tests := make([]Test, 0)
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return tests, err
+	}
+	err = json.Unmarshal(bytes, &tests)
+	if err == nil {
+		log.Debug().Msgf("Loaded Tests index: %+v", tests)
+	}
+	return tests, err
+}
+
+func checkDir(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Error().Str("path", path).Err(err).Msg("Error opening")
+		return err
+	}
+	fs, err := f.Stat()
+	if err != nil {
+		log.Error().Str("path", path).Err(err).Msg("Error stat(ing?)")
+		return err
+	}
+	if fs.IsDir() {
+		return nil
+	}
+	return err
+}
--- src/default.go
+++ src/default.go
@@ -1,76 +1,41 @@
 package main
 
 import (
-	"github.com/philandstuff/dhall-golang/v4"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"io/ioutil"
 	"os"
 )
-
-type WoTImplementation struct {
-	name    string
-	path    string // this is the "install" or "library" path, not the path of the
-	runtime Runtime
-}
-
-type Config struct {
-	TestsDir           string
-	ImplementationsDir string
-	LogLevel           int8
-	Implementations    []WoTImplementation
-}
-
-func loadConfig(configPath string) (Config, error) {
-	var config Config
-	bytes, err := ioutil.ReadFile(configPath)
-	if err != nil {
-		return config, err
-	}
-	err = dhall.Unmarshal(bytes, &config)
-	if err == nil {
-		log.Debug().Msgf("Loaded Config: %+v", config)
-	}
-	return config, err
-}
-
-func checkDir(path string) error {
-	f, err := os.Open(path)
-	if err != nil {
-		log.Error().Str("path", path).Err(err).Msg("Error opening")
-	}
-	fs, err := f.Stat()
-	if err != nil {
-		log.Error().Str("path", path).Err(err).Msg("Error stat(ing?)")
-	}
-	if fs.IsDir() {
-		return nil
-	}
-	return err
-}
 
 func main() {
 	configPath := "config.d"
 	config, err := loadConfig(configPath)
 	if err != nil {
-		zerolog.SetGlobalLevel(zerolog.Level(config.LogLevel))
 		log.Error().Msgf("Failed to load/parse config file (%s): %s", configPath, err)
 		os.Exit(1)
 	}
+	zerolog.SetGlobalLevel(zerolog.Level(config.LogLevel))
+
+	tests := make(map[string][]Test)
 
 	for _, i := range config.Implementations {
-		log.Debug().Str("Scanning if ImplementationsDir and TestDir exist for implementation", i.name)
-		path := config.ImplementationsDir + "/" + i.name
+		log.Debug().Str("Scanning if ImplementationsDir and TestDir exist for implementation", i.Name)
+		path := config.ImplementationsDir + "/" + i.Name
 		err := checkDir(path)
 		if err != nil {
 			log.Error().Err(err).Str("path", path).Msg("Error checking implementation folder")
 		}
-		path = config.TestsDir + "/" + i.name
+		path = config.TestsDir + "/" + i.Name
 		err = checkDir(path)
 		if err != nil {
 			log.Error().Err(err).Str("path", path).Msg("Error checking test folder")
 		}
+		// dhall would have been much nicer, but optional stuff is hard and I'm not good enough at writing it
+		t, err := loadTestIndex(path + "/index.json")
+		if err != nil {
+			log.Error().Err(err).Str("path", path+"/index.d").Msg("Error loading test index")
+		}
+		tests[i.Name] = t
 	}
 
-	runTests(config)
+	runTests(config, tests)
 }
