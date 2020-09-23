464da67cd202456ec2596e36fb64af632ba65d6c
ajs124 <git@ajs124.de>
2020-09-23 04:32:13 +0200

    runner: refactor and start implementation of server protocol tests

--- src/runner.go
+++ src/runner.go
@@ -3,27 +3,15 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/ajs124/WoTest/clients"
+	"github.com/ajs124/WoTest/servers"
 	"github.com/rs/zerolog/log"
 	"io"
-	"io/ioutil"
+	"net/http"
 	"net/url"
 	"os/exec"
 	"regexp"
 	"time"
-)
-
-const (
-	ModeClient = iota
-	ModeServer
-)
-
-const (
-	ProtoHttp = iota
-	ProtoHttps
-	ProtoCoap
-	ProtoMqtt
 )
 
 type TestResult struct {
@@ -32,60 +20,156 @@ type TestResult struct {
 	succeeded bool
 }
 
-func waitForOutput(out []io.ReadCloser, match string) (bool, error) {
-	cOut := make(chan bool)
-	cErr := make(chan bool)
-	go func(c chan bool) {
-		r, err := regexp.Compile(match)
-		buf := make([]byte, 0)
-		n, err := out[0].Read(buf)
-		r.Match(buf)
-	}(cOut)
+func (res *TestResult) waitForOutput(out []io.ReadCloser, match string) (bool, error) {
+	rexpr, err := regexp.Compile(match)
+	if err != nil {
+		log.Err(err)
+	}
+	buf := make([]byte, 4096)
+	err = nil
+	m := false
+	n := 0
+	sout := make([][]byte, 2)
+	for err == nil && !m {
+		for j, o := range out {
+			n, err = o.Read(buf)
+			if err != nil {
+				log.Debug().Err(err).Msg("Error while trying to read")
+				break
+			}
+			i := 0
+			for i < n {
+				sout[j] = append(sout[j], buf[i])
+				i++
+			}
+			/* if n > 0 {
+				log.Debug().Int("n", n).Str("now", string(sout[j])).Msg("Read bytes")
+			} */
+			if rexpr.Match(sout[j]) {
+				m = true
+				break
+			}
+		}
+		time.Sleep(10 * time.Millisecond) // FIXME: why?
+	}
+	res.stdout += string(sout[0])
+	res.stderr += string(sout[1])
+	return m, err
 }
 
-func runTest(test Test, impl WoTImplementation, config Config, execFunc func(string, string, context.Context, ...string) (*exec.Cmd, []io.ReadCloser, error)) (TestResult, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(test.Timeout))
+func runProtocolClientTest(test Test, impl WoTImplementation, config Config, execFunc func(string, string, context.Context, ...string) (*exec.Cmd, []io.ReadCloser, error)) (TestResult, error) {
+	result := TestResult{"", "", false}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(test.Timeout))
 	defer cancel()
-	_, out, err := execFunc(impl.Path, config.TestsDir+"/"+impl.Name, ctx, test.Path)
+	cmd, out, err := execFunc(impl.Path, config.TestsDir+"/"+impl.Name, ctx, test.Path)
 	if err != nil {
 		log.Error().Err(err).Str("name", impl.Name).Msg("Starting test command failed")
 	}
-	time.Sleep(time.Second * 1)
+
+	found, err := result.waitForOutput(out, ".* ready.*")
+	log.Debug().Bool("found", found).Msg("ready")
+
+	reqUrl, _ := url.Parse(test.ProtocolTestProperties.RequestUrl)
+
+	var client clients.Client
+
+	if test.ProtocolTestProperties.Protocol == ProtoHttp || test.ProtocolTestProperties.Protocol == ProtoHttps {
+		client = &clients.HttpClient{}
+		r, err := client.Recv(*reqUrl)
+		if err != nil {
+			log.Err(err).Msg("http client error")
+		} else {
+			result.succeeded = true
+			log.Debug().Str("http client output", string(r))
+		}
+	} else if test.ProtocolTestProperties.Protocol == ProtoCoap {
+		client = &clients.CoapClient{}
+		_, err = client.Connect(*reqUrl)
+		if err != nil {
+			log.Err(err).Msg("coap client failed to connect")
+		}
+		r, err := client.Recv(*reqUrl)
+		if err != nil {
+			log.Err(err).Msg("coap client failed to GET data")
+		} else {
+			result.succeeded = true
+			log.Debug().Str("coap client output", string(r))
+		}
+	} else if test.ProtocolTestProperties.Protocol == ProtoMqtt {
+		return result, errors.New("not implemented")
+	} else {
+		// wtf?
+		panic(errors.New("test has invalid protocol"))
+	}
+
+	err = cmd.Process.Kill()
+	return result, nil
+}
+
+func runProtocolServerTest(test Test, impl WoTImplementation, config Config, execFunc func(string, string, context.Context, ...string) (*exec.Cmd, []io.ReadCloser, error)) (TestResult, error) {
+	result := TestResult{"", "", false}
+	var err error
+
+	if test.ProtocolTestProperties.Protocol == ProtoHttp || test.ProtocolTestProperties.Protocol == ProtoHttps {
+		var server servers.HttpServer
+		err = server.Listen(test.ProtocolTestProperties.ServeAt)
+		defer server.Stop()
+		if err != nil {
+			log.Err(err).Msg("http server error")
+		}
+
+		server.SetHandleFunc(func(w http.ResponseWriter, r *http.Request) {
+			// TODO: add support for other types of content
+			http.ServeFile(w, r, test.ProtocolTestProperties.ServeContent)
+		})
+	} else if test.ProtocolTestProperties.Protocol == ProtoCoap {
+		var server servers.CoapServer
+		err = server.Listen(test.ProtocolTestProperties.ServeAt)
+		defer server.Stop()
+		if err != nil {
+			log.Err(err).Msg("coap server error")
+		}
+	} else if test.ProtocolTestProperties.Protocol == ProtoMqtt {
+		return result, errors.New("not implemented")
+	} else {
+		// wtf?
+		panic(errors.New("test has invalid protocol"))
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*time.Duration(test.Timeout))
+	defer cancel()
+	cmd, out, err := execFunc(impl.Path, config.TestsDir+"/"+impl.Name, ctx, test.Path)
+	if err != nil {
+		log.Error().Err(err).Str("name", impl.Name).Msg("Starting test command failed")
+	}
+
+	matches := 0
+	for _, mM := range test.ProtocolTestProperties.MustMatch {
+		found := false
+		found, err = result.waitForOutput(out, mM)
+		log.Debug().Bool("found", found).Msg(mM)
+		if found {
+			matches += 1
+		}
+	}
+	result.succeeded = len(test.ProtocolTestProperties.MustMatch) == matches
+	err = cmd.Process.Kill()
+	return result, err
+}
+
+func runTest(test Test, impl WoTImplementation, config Config, execFunc func(string, string, context.Context, ...string) (*exec.Cmd, []io.ReadCloser, error)) (TestResult, error) {
 	if test.Type == TestTypeProtocol {
 		if test.ProtocolTestProperties.Mode == ModeClient {
-			reqUrl, _ := url.Parse(test.ProtocolTestProperties.RequestUrl)
-			if test.ProtocolTestProperties.Protocol == ProtoHttp || test.ProtocolTestProperties.Protocol == ProtoHttps {
-				var client clients.HttpClient
-				r, err := client.Recv(*reqUrl)
-				if err != nil {
-					fmt.Print(err)
-				}
-				fmt.Print(string(r) + "\n")
-			} else if test.ProtocolTestProperties.Protocol == ProtoCoap {
-				var client clients.CoapClient
-				client.Connect(*reqUrl)
-				r, err := client.Recv(*reqUrl)
-				if err != nil {
-					fmt.Print(err)
-				}
-				fmt.Print(string(r) + "\n")
-			} else if test.ProtocolTestProperties.Protocol == ProtoMqtt {
-
-			} else {
-				// wtf?
-				panic(errors.New("test has invalid protocol"))
-			}
+			return runProtocolClientTest(test, impl, config, execFunc)
 		} else if test.ProtocolTestProperties.Mode == ModeServer {
-
+			return runProtocolServerTest(test, impl, config, execFunc)
 		} else {
 			// wtf?
 			panic(errors.New("test has invalid mode"))
 		}
 	}
-	waitForOutput(out)
-	cmd.Process.Kill()
-	result := TestResult{string(stdout), string(stderr), true}
-	return result, nil
+	panic(errors.New("cannot run this type of test"))
 }
 
 func runTests(config Config, tests map[string][]Test) {
@@ -104,7 +188,18 @@ func runTests(config Config, tests map[string][]Test) {
 
 		for _, test := range tests[impl.Name] {
 			result, err := runTest(test, impl, config, execFunc)
-			if err != nil {
+			fields := make(map[string]interface{})
+			if test.Type == TestTypeProtocol {
+				fields["protocol"] = test.ProtocolTestProperties.Protocol
+				fields["mode"] = test.ProtocolTestProperties.Mode
+			}
+			logResult.Log().Bool("succeeded", result.succeeded).
+				Str("stdout", result.stdout).
+				Str("stderr", result.stderr).
+				Str("path", test.Path).
+				Fields(fields).
+				Uint("type", test.Type).Msg("")
+			if err != nil || !result.succeeded {
 				log.Error().
 					Err(err).
 					Str("path", test.Path).
