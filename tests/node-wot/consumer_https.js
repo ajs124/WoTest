9d01878a564f1934ef851a2f19d644618296e492
ajs124 <git@ajs124.de>
2020-09-24 02:49:12 +0200

    tests/node-wot: change some stuff around

--- tests/node-wot/consumer_https.js
+++ tests/node-wot/consumer_https.js
@@ -4,7 +4,11 @@ HttpsClientFactory = require("@node-wot/binding-http").HttpsClientFactory
 
 let servient = new Servient();
 let wotHelper = new WotCore.Helpers(servient);
-servient.addClientFactory(new HttpsClientFactory());
+servient.addClientFactory(new HttpsClientFactory({
+    "config": {
+        "allowSelfSigned": true
+    }
+}));
 
 wotHelper.fetch("https://127.0.0.1:8081/").then(async (td) => {
     let w = await servient.start();
