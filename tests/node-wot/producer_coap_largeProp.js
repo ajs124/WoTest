980efa88d46c9a86594615bdd5162118ced6e464
ajs124 <git@ajs124.de>
2020-09-30 17:50:02 +0200

    tests/node-wot: and here as well

---
+++ tests/node-wot/producer_coap_largeProp.js
@@ -0,0 +1,40 @@
+Servient = require("@node-wot/core").Servient
+CoapServer = require("@node-wot/binding-coap").CoapServer
+Helpers = require("@node-wot/core").Helpers
+
+let servient = new Servient();
+servient.addServer(new CoapServer({
+    "port": "8080",
+}));
+
+servient.start().then((WoT) => {
+    WoT.produce({
+        "@context": "https://www.w3.org/2019/wot/td/v1",
+        "title": "coap_largeProp",
+        "properties": {
+            "bench": {
+                "title": "download this",
+                "type": "string"
+            }
+        }
+    }).then((thing) => {
+        console.log("Produced " + thing.getThingDescription().title);
+        let goalSize = parseInt(process.argv[2]);
+        let buf = Buffer.alloc(goalSize);
+        for (i=0; i < goalSize; ++i) {
+            let r = Math.floor(Math.random() * Math.floor(255));
+            buf.write(r.toString(16), i);
+        }
+        console.log("Generated " + buf.length + " character long random string")
+        thing.setPropertyReadHandler("bench", (options) => {
+            return new Promise ((resolve, reject) => {
+                resolve(buf.toString());
+            });
+        });
+
+        thing.expose().then(() => {
+            console.info(thing.getThingDescription().title + " ready");
+            console.info("TD: " + JSON.stringify(thing.getThingDescription()));
+        });
+    });
+});
